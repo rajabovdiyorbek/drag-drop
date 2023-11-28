c8976e278c0316505885131a3932f60c8692f4f3
rajabovdiyorbek <m.mirzajanov@seclgroup.com>
2023-11-28 18:24:29 +0500

    some changes

--- src/components/DraggableBlock.vue
+++ src/components/DraggableBlock.vue
@@ -1,18 +1,28 @@
 <template>
 <div class="container">
-    <draggable v-model="myArray" @start="onDragStart" @end="onDragEnd">
+    <draggable v-model="myArray" v-bind="dragOptions" @start="onDragStart"  @end="onDragEnd">
       <div
         v-for="(element, index) in myArray"
         :key="element.id"
-        class="card"
+        class="cards"
         @dragenter="dragEnter"
         @dragleave="dragLeave"
+        @dragover="dragOver"
       >
         <div v-if="isDragging && index === draggedIndex" class="ghost">
             <span class="card--number">{{ index + 1 }}</span>
-            <img src="../assets/icons/plus.svg" alt="plus-icon" />
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g id="icon" clip-path="url(#clip0_43_6815)">
+            <path id="Icon" fill-rule="evenodd" clip-rule="evenodd" d="M8 -0.5C8.55229 -0.5 9 -0.0522848 9 0.5V7H15.5C16.0523 7 16.5 7.44772 16.5 8C16.5 8.55229 16.0523 9 15.5 9H9V15.5C9 16.0523 8.55229 16.5 8 16.5C7.44772 16.5 7 16.0523 7 15.5V9H0.5C-0.0522848 9 -0.5 8.55229 -0.5 8C-0.5 7.44772 -0.0522848 7 0.5 7H7V0.5C7 -0.0522848 7.44772 -0.5 8 -0.5Z" fill="#FF1919"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_43_6815">
+              <rect width="16" height="16" fill="white"/>
+            </clipPath>
+          </defs>
+        </svg>
         </div>
-        <div >
+        <div class="card">
           <img :src="element.photo" :alt="element.photo" class="card--image" />
           <span class="card--number">{{ index + 1 }}</span>
           <img v-if="!isChosen" class="card--more" src="../assets/icons/more.svg">
@@ -20,7 +30,16 @@
       </div>
   </draggable>
   <div class="card--image__add">
-      <img class="plus" src="../assets/icons/gray-plus.svg">
+    <svg  width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g id="icon" clip-path="url(#clip0_43_6815)">
+    <path id="Icon" fill-rule="evenodd" clip-rule="evenodd" d="M8 -0.5C8.55229 -0.5 9 -0.0522848 9 0.5V7H15.5C16.0523 7 16.5 7.44772 16.5 8C16.5 8.55229 16.0523 9 15.5 9H9V15.5C9 16.0523 8.55229 16.5 8 16.5C7.44772 16.5 7 16.0523 7 15.5V9H0.5C-0.0522848 9 -0.5 8.55229 -0.5 8C-0.5 7.44772 -0.0522848 7 0.5 7H7V0.5C7 -0.0522848 7.44772 -0.5 8 -0.5Z" fill="#C4C9D4"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_43_6815">
+      <rect width="16" height="16" fill="white"/>
+    </clipPath>
+  </defs>
+</svg>
       <span class="text">Добавить фото</span>
   </div>
 </div>
@@ -36,6 +55,7 @@ export default {
   },
   data() {
     return {
+      startY: 0,
       myArray: [
         { id: 1, photo: require('@/assets/img1.jpeg') },
         { id: 2, photo: require('@/assets/img2.jpeg') },
@@ -48,7 +68,10 @@ export default {
         { id: 9, photo: require('@/assets/img1.jpeg') },
 
       ],
-      selectedCard: null,
+      dragOptions: {
+        swapThreshold: 1,
+        animation: 150,
+      },
       isDragging: false,
       isChosen: false,
       draggedIndex: -1,
@@ -56,7 +79,9 @@ export default {
   },
   methods: {
     onDragStart(evt) {
+      this.isChosen = true
       this.draggedIndex = evt.oldIndex;
+      this.startY = evt.originalEvent.clientY
     },
     onDragEnd() {
       this.isDragging = false;
@@ -64,14 +89,18 @@ export default {
       this.draggedIndex = -1;
     },
     dragEnter(evt) {
-      this.isChosen = true
       if (evt.relatedTarget && !evt.currentTarget.contains(evt.relatedTarget)) {
+        this.isDragging = true;
+      }
+    },
+    dragOver(evt) {
+      if (evt.clientY - this.startY > 40) {
         this.isDragging = true;
       }
     },
     dragLeave(evt) {
       if (evt.relatedTarget === null || (evt.currentTarget && !evt.currentTarget.contains(evt.relatedTarget))) {
-        this.isDragging = false;
+        this.isDragging = true;
       }
     },
   },
@@ -82,7 +111,7 @@ export default {
 .container {
   display: flex;
 }
-.card {
+.cards {
   position: relative;
   border: 1px solid #C4C9D4;
   margin: 1px;
@@ -95,7 +124,7 @@ export default {
   background: #fff;
   cursor: pointer;
 }
-.card:hover {
+.cards:hover {
   border: 1px solid #578FC7;
 }
 
@@ -116,6 +145,15 @@ export default {
   border-radius: 4px;
   border: 1px dashed #C4C9D4;
   cursor: pointer;
+}
+
+.card--image__add:hover {
+  border-color: #578FC7;
+  color: #578FC7;
+}
+
+.card--image__add:hover svg path {
+  fill: #578FC7;
 }
 
 .text {
@@ -142,24 +180,35 @@ export default {
 }
 .card--more {
   position: absolute;
-  width: 16px;
+  width: 20px;
   top: 5px;
   right: 5px;
   background: white;
-  padding: 4px;
+  padding: 2px;
   font-size: 14px;
   cursor: pointer;
+}
+.sortable-chosen .card--more {
+  display: none;
 }
 .sortable-chosen {
   border: 1px solid #FF1919 !important;
 }
 
-.sortable-chosen .card--more {
+.sortable-chosen .card--number {
   display: none;
 }
 .sortable-chosen.sortable-ghost {
+  transform: none !important;
   border: none !important;
   transition: none;
+}
+
+.sortable-chosen.sortable-ghost .card {
+  opacity: 0.4;
+}
+.sortable-chosen.sortable-ghost .card--number {
+  display: block;
 }
 
 .ghost {
@@ -172,6 +221,7 @@ export default {
   height: 144px;
   border-radius: 4px;
   border: 1px dashed #FF1919;
+  z-index: 3;
 }
 
 </style>
